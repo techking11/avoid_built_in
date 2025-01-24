ee1c23e574a5672a0ec3d54fc10b15074bd73691
techking11 <nma1132000@gmail.com>
2025-01-24 11:23:20 +0700

    add min of nums

---
+++ min.ts
@@ -0,0 +1,15 @@
+function min(...nums: number[]): number {
+  let min: number = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0 || min > nums[i]) {
+      min = nums[i];
+    }
+  }
+  console.log(min);
+  return min;
+}
+
+min(8, 3, 2, 5, 10, 7);
+min(3, 4, 5, 8, 9, 3, 2);
+min(10, 11);
+min(800, 900, 700, 300, 500);
