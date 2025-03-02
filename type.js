3c91c112065f347fd68a7b933cd3f05cfbf572be
techking11 <nma1132000@gmail.com>
2025-03-02 12:47:24 +0700

    update and code refactor for numeric syntax

--- type.js
+++ type.js
@@ -1,6 +1,9 @@
 /**
  * Primitive Types: Number, String, Boolean, undefined, null, Symbol, BigInt
  * Reference Types: Object, Array, Function, Date, RegExp, Error
+ *
+ * It was once said, “JavaScript is the only language developers don’t
+ * learn to use before using it.”
  */
 
 // Output: object -> legal issue in js -> null means no object
@@ -15,3 +18,16 @@ console.log(typeof [1, 2, 3]); // output: object
 
 var d;
 console.log(typeof d); // output: underfined
+// variable don't have type, value have type
+
+typeof b; // Reference error: b is not defined
+// is not defined, and underfine is different :)
+// if undeclared in browser, itd be nice
+
+// numeric syntax // base-10 decimals literally
+var a = 0.43;
+var b = 42; // if 0, optional
+var e = 5e10; // 5 * 10^10
+// it works 43 .toFixed(3), (43).toFixed(3) but 43.toFixed(3) has error
+// strict or non-stict
+console.log(a + b);
